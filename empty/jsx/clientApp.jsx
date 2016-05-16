8e3afb9e57e0bfb4f019fa8898474f5fd0b7f448
Brian Holt <btholt@gmail.com>
2016-05-16 14:45:55 -0500

    os1

---
+++ empty/jsx/clientApp.jsx
@@ -0,0 +1,4 @@
+import { render } from 'react-dom';
+import React from 'react';
+import App from './index';
+render(<App />, document.querySelector('#target'));
---
+++ empty/jsx/index.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { clone } from 'lodash';
+import { Search } from './netflix';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      results: clone(Search)
+    };
+  }
+  render () {
+    return (
+      <div className='app-container'>
+        <div className='movies-list'>
+          <pre>
+            <code>{JSON.stringify(this.state.results,null,4)}</code>
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
